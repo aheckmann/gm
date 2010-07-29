a9aaeb9549f742ceec6ccfa895a165de5ca18b61
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2010-07-29 17:51:08 -0400

    move helper methods into convenience dir

--- index.js
+++ index.js
@@ -16,6 +16,5 @@ function gm(source){
 
 require("./lib/getters")(gm.prototype)
 require("./lib/args")(gm.prototype)
-require("./lib/thumb")(gm.prototype)
-require("./lib/morph")(gm.prototype)
+require("./lib/convenience")(gm.prototype)
 require("./lib/command")(gm.prototype)
