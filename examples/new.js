8449aacc27840dd422fac91b7e82f01a34c272ef
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2010-09-06 01:06:11 -0400

    add example for creating images from scratch

---
+++ examples/new.js
@@ -0,0 +1,18 @@
+
+// gm - Copyright Aaron Heckmann <aaron.heckmann+github@gmail.com> (MIT Licensed)
+
+var gm = require('../')
+  , dir = __dirname + '/imgs'
+
+gm.new(525, 110, "#00ff55aa")
+  .fontSize(68)
+  .stroke("#efe", 2)
+  .fill("#555")
+  .drawText(20, 72, "graphics")
+  .fill("#fa0")
+  .drawText(274, 72, " magick")
+  .write(dir + '/new.png', function(err){
+    if (err) return console.dir(arguments)
+    console.log(this.outname + ' created  :: ' + arguments[3])
+  }
+) 
