2c85dfd83823f9aaa84ef5f667930931a0123f4f
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2011-04-28 17:01:27 -0400

    refactor

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 
 // gm - Copyright Aaron Heckmann <aaron.heckmann+github@gmail.com> (MIT Licensed)
 
-module.exports = gm
+/**
+ * Constructor.
+ *
+ * @param {String|Number} path - path to img source or width of img to create
+ * @param {Number} [height] - optional height of img to create
+ * @param {String} [color] - optional hex background color of created img
+ */
 
-function gm(source, height, color){
-  if (!(this instanceof gm))
-    return new gm(source, height, color)
+function gm (source, height, color) {
+  var width;
 
-  this.data = {}
-  this._in = []
-  this._out = []
-
-  var width
-
-  if (height){
-    // new images
-    width = source
-    source = ""
-    this.arg(
-      [ "-size", width + "x" + height ]
-      .concat( color ? ['"xc:'+ color + '"'] : [] )
-    )
+  if (!(this instanceof gm)) {
+    return new gm(source, height, color);
   }
 
-  this.source = source
+  this.data = {};
+  this._in = [];
+  this._out = [];
+
+  if (height) {
+    // new images
+    width = source;
+    source = "";
+
+    var arg = ["-size", width + "x" + height];
+
+    if (color) {
+      arg = arg.concat(['"xc:'+ color + '"']);
+    }
+
+    this.arg(arg);
+  }
+
+  this.source = source;
 }
-  
-require("./lib/getters")(gm.prototype)
-require("./lib/args")(gm.prototype)
-require("./lib/drawing")(gm.prototype)
-require("./lib/convenience")(gm.prototype)
-require("./lib/command")(gm.prototype)
+
+/**
+ * Augment the prototype.
+ */
+
+require("./lib/getters")(gm.prototype);
+require("./lib/args")(gm.prototype);
+require("./lib/drawing")(gm.prototype);
+require("./lib/convenience")(gm.prototype);
+require("./lib/command")(gm.prototype);
+
+/**
+ * Expose.
+ */
+
+module.exports = gm;
 
