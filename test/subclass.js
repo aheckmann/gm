92f0a4ed49d8ba4dc533d3fd51e219dd04a85aac
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-01-11 08:46:01 -0500

    better subclass test

--- test/subclass.js
+++ test/subclass.js
@@ -13,10 +13,13 @@ module.exports = function (_, dir, finish, gm) {
 
   assert.equal('gm', sub('test').constructor.name);
   assert.equal(true, sub.prototype._options.imageMagick);
+  assert.equal(undefined, gm.prototype._options.imageMagick);
 
   var s = sub('test');
   assert.equal(true, s._options.imageMagick);
-  assert.equal(undefined, gm.prototype._options.imageMagick);
+
+  var g = gm('test');
+  assert.equal(undefined, g._options.imageMagick);
 
   gm(dir + '/photo.JPG')
   .thumb(50, 80, dir + '/SUBthumb.png', function subthumb (err) {
