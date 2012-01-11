c292685b96e279f428d9001c9b3f4302106685ae
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-01-11 10:24:22 -0500

    add imagemagick support
    closes #23
    closes #29

--- test/options.js
+++ test/options.js
@@ -3,6 +3,7 @@
 
 
 var assert = require('assert')
+  , fs = require('fs')
 
 module.exports = function (_, dir, finish, gm) {
 
@@ -17,9 +18,44 @@ module.exports = function (_, dir, finish, gm) {
   assert.equal(undefined, s._options.hellowwww);
   assert.equal(undefined, g._options.subclassed);
 
+  /// subclass options
   var s2 = sub('another');
   assert.equal(true, s2._options.subclassed);
   assert.equal(undefined, s2._options.setWithMethod);
 
-  finish();
+  var writeFile = dir + '/IM-negative.png';
+  var imageMagick = gm.subClass({ imageMagick: true });
+
+  imageMagick(dir + '/photo.JPG')
+  .negative()
+  .write(writeFile, function (err, _1, _2, cmd) {
+    if (err) return finish(err);
+
+    assert.ok(/^convert /.test(cmd));
+
+    fs.stat(writeFile, function (err) {
+      if (err) return finish(new Error('imagemagick did not write file'));
+      try {
+        fs.unlinkSync(writeFile);
+      } catch (e) {}
+
+      /// inline options
+      gm(dir + '/photo.JPG')
+      .negative()
+      .options({ imageMagick: true })
+      .write(writeFile, function (err, _1, _2, cmd) {
+        if (err) return finish(err);
+
+        assert.ok(/^convert /.test(cmd));
+
+        fs.stat(writeFile, function (err) {
+          if (err) return finish(new Error('imagemagick did not write file'));
+          try {
+            fs.unlinkSync(writeFile);
+          } catch (e) {}
+          finish();
+        });
+      });
+    });
+  });
 }
