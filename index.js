2f06fbb7d50acd990b3ef2d47d1860b3fda5258e
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-01-26 16:27:47 -0500

    duh

--- index.js
+++ index.js
@@ -92,5 +92,4 @@ module.exports = exports = gm;
 module.exports.version = JSON.parse(
   require('fs').readFileSync(__dirname + '/package.json', 'utf8')
 ).version;
-console.error(module.exports.version);
 
