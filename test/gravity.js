d4c0202d47f0aa48e1e86ebc1c72ece1d2297a80
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-03-30 23:36:35 -0400

    validate gravity

--- test/gravity.js
+++ test/gravity.js
@@ -1,6 +1,10 @@
-module.exports = function (gm, dir, finish) {
+var assert = require('assert')
 
-  gm
+module.exports = function (img, dir, finish, gm) {
+  var changed = gm('whatever.png').gravity("Souths")
+  assert.equal(changed._out[1], 'NorthWest');
+
+  img
   .scale(200, 100)
   .gravity("South")
   .extent(300,300)
