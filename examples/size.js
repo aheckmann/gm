bb3a518e138ea00f4abcc83c9ba7895f7f4a0b0a
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-08-17 13:34:25 -0700

    added; size example

---
+++ examples/size.js
@@ -0,0 +1,18 @@
+
+// gm - Copyright Aaron Heckmann <aaron.heckmann+github@gmail.com> (MIT Licensed)
+
+var gm = require('../')
+  , dir = __dirname + '/imgs'
+
+var methods = 
+[ "size"
+]
+
+var image = gm(dir + '/original.png')
+methods.forEach(function(method){
+  image[method](function(err, result){
+    console.log(method + " result:")
+    console.dir(result)
+  })
+})
+
