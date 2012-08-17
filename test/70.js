e04a0bb265434a056932723d3dcd649d337d605c
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-08-17 13:34:14 -0700

    added; test for #70

---
+++ test/70.js
@@ -0,0 +1,42 @@
+
+// gm - Copyright Aaron Heckmann <aaron.heckmann+github@gmail.com> (MIT Licensed)
+var assert = require('assert')
+var times = 16;
+
+var loading = __dirname + '/fixtures/loading.gif'
+var favicon = __dirname + '/fixtures/favicon.png'
+
+module.exports = function (_, dir, finish, gm) {
+  var pending = times/2;
+
+  var res = {};
+  res[loading] = {};
+  res[favicon] = {};
+
+  function done (err){
+    if (err) return finish(done.ran = err);
+    if (done.ran) return;
+    if (--pending) return;
+    finish();
+  }
+
+  new Array(times).join('x').split('x').forEach(function (_, i) {
+    ;[loading, favicon].forEach(function (img) {
+      gm(img)
+      .size(function edge (err, size) {
+        if (err) return done(err);
+
+        'width height'.split(' ').forEach(function (prop) {
+          if (!(prop in res[img])) {
+            res[img][prop] = size[prop];
+          } else {
+            assert.equal(res[img][prop], size[prop]);
+          }
+        });
+
+        done(err);
+      });
+    });
+  });
+
+}
