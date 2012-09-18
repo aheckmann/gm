d81b17a5949355572feababc3984e50e05469f7e
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-09-17 21:55:25 -0700

    fix test

---
+++ test/78.js
@@ -0,0 +1,25 @@
+
+var assert = require('assert')
+
+module.exports = function (_, dir, next, gm) {
+  var magick = _._options.imageMagick;
+  var name = magick ? '78-IM' : '78';
+  var out = dir + '/' + name;
+
+  _.resize(600, 450, '!').write(out + '.png', function (err) {
+    if (err) return next(err);
+
+    var img = gm(out + '.png');
+    if (magick)
+      img.options({ imageMagick: true });
+
+    img
+    .crop(70, 70, 100, 100)
+    .resize(50, 50)
+    .write(out + '-2.jpg', function (err) {
+      if (err) return next(err);
+      next();
+    })
+  });
+
+}
