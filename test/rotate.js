b7c35ab2db0bfba0014b3c567916d651dce1d989
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-11-10 07:27:39 -0800

    fixed; rotation by 0
    closes #90

--- test/rotate.js
+++ test/rotate.js
@@ -1,11 +1,22 @@
 
 // gm - Copyright Aaron Heckmann <aaron.heckmann+github@gmail.com> (MIT Licensed)
 
+var assert = require('assert')
+
 module.exports = function (gm, dir, finish) {
 
   gm
   .rotate('red', -40)
-  .write(dir + '/rotate.png', function rotate (err) {
-    finish(err);
+  .write(dir + '/rotate.png', function rotate (err, _0, _1, cmd) {
+    assert.ok(/"-rotate" "-40"/.test(cmd));
+
+    gm
+    .rotate('red', 0)
+    .write(dir + '/rotate.png', function rotate (err, _0, _1, cmd) {
+      assert.ok(!/"-rotate" "-40"/.test(cmd));
+      assert.ok(/"-rotate" "0"/.test(cmd));
+      finish(err);
+    });
+
   });
 }
