de52b30741f09353aeca1f7c4cc8d2246ddcddfb
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-12-14 09:24:57 -0800

    added; geometry support to -scale
    relates to #98

--- test/scale.js
+++ test/scale.js
@@ -3,6 +3,24 @@ var assert = require('assert')
 
 module.exports = function (gm, dir, finish, GM) {
 
+  var a = GM('img.png').scale(100);
+  var args = a.args();
+  assert.equal('convert', args[0]);
+  assert.equal('-scale', args[2]);
+  assert.equal('100', args[3]);
+
+  var a = GM('img.png').scale(100, 200, '%');
+  var args = a.args();
+  assert.equal('convert', args[0]);
+  assert.equal('-scale', args[2]);
+  assert.equal('100x200%', args[3]);
+
+  var a = GM('img.png').scale(100, '200%');
+  var args = a.args();
+  assert.equal('convert', args[0]);
+  assert.equal('-scale', args[2]);
+  assert.equal('100x200%', args[3]);
+
   var m = gm
   .scale(100, 100);
 
