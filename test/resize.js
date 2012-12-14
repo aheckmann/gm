3d7a59c7800ac73cc8d45b3d0800d2ba62c66403
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-12-14 09:09:05 -0800

    fixed; no long include -size with resize()
    add it manually if you need it
    
    closes #98

--- test/resize.js
+++ test/resize.js
@@ -3,15 +3,37 @@ var assert = require('assert')
 
 module.exports = function (gm, dir, finish, GM) {
 
+  var a = GM('img.png').resize(10);
+  var args = a.args();
+  assert.equal('convert', args[0]);
+  assert.equal('-resize', args[2]);
+  assert.equal('10', args[3]);
+
+  var a = GM('img.png').resize(10, 20);
+  var args = a.args();
+  assert.equal('convert', args[0]);
+  assert.equal('-resize', args[2]);
+  assert.equal('10x20', args[3]);
+
+  var a = GM('img.png').resize(10, false, '%');
+  var args = a.args();
+  assert.equal('convert', args[0]);
+  assert.equal('-resize', args[2]);
+  assert.equal('10%', args[3]);
+
+  var a = GM('img.png').resize('10%');
+  var args = a.args();
+  assert.equal('convert', args[0]);
+  assert.equal('-resize', args[2]);
+  assert.equal('10%', args[3]);
+
   var m = gm
   .resize(58, 50, '%');
 
   var args=  m.args();
   assert.equal('convert', args[0]);
-  assert.equal('-size', args[1]); // TODO remove this from resize() method
-  assert.equal('58x50%', args[2]);
-  assert.equal('-resize', args[4]);
-  assert.equal('58x50%', args[5]);
+  assert.equal('-resize', args[2]);
+  assert.equal('58x50%', args[3]);
 
   if (!GM.integration)
     return finish();
