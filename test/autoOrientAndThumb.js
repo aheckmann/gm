1344dcd56e13eb42495b8cd8c5066469452df33e
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-12-21 14:35:41 -0800

    tests; add autoOrient + thumb() test
    closes #99
    fix was in #105

---
+++ test/autoOrientAndThumb.js
@@ -0,0 +1,39 @@
+var assert = require('assert')
+var fs = require('fs')
+
+module.exports = function (_, dir, finish, gm) {
+  if (!gm.integration)
+    return finish();
+
+  var original = dir + '/orientation/Portrait_7.jpg';
+  var result = dir + '/autoOrientAndThumb.png';
+
+  size(original, function (err, origSize) {
+    if (err) return finish(err);
+
+    assert.ok(origSize.width < 610);
+    assert.ok(origSize.height < 460);
+
+    var m = gm(original)
+    .autoOrient()
+    .thumb(100, 100, result, function (err) {
+      if (err) return finish(err);
+
+      size(result, function (err, newSize) {
+        if (err) return finish(err);
+        assert.ok(newSize.width < 80);
+        assert.ok(newSize.height < 110);
+        finish();
+      });
+
+    });
+  });
+
+
+  function size (file, cb) {
+    gm(file).identify(result, function (err, data) {
+      if (err) return cb(err);
+      cb(err, data.size);
+    });
+  }
+}
