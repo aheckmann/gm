20ba5c46849df94acf4f89011a8079b7a81d9aa5
Jonathan Ong <jonathanrichardong@gmail.com>
2013-04-08 18:21:01 -0700

    remove bufferStream option

--- test/utils.js
+++ test/utils.js
@@ -3,7 +3,6 @@ var assert = require('assert')
 
 module.exports = function (_, dir, finish, gm) {
 
-  assert.equal('function', typeof gm.utils.buffer);
   assert.equal('function', typeof gm.utils.escape);
   finish();
 
