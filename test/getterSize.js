846b8d846c0813551d1e8ad335d872042b7bcff6
Jonathan Ong <me@jongleberry.com>
2013-04-21 02:02:38 -0700

    tests: getter size should test GIF and PNG as well

--- test/getterSize.js
+++ test/getterSize.js
@@ -1,20 +1,53 @@
 
 var assert = require('assert');
 
-module.exports = function (gm, dir, finish, GM) {
-  if (!GM.integration)
+module.exports = function (_, dir, finish, gm) {
+  if (!gm.integration)
     return finish();
 
-  gm
-  .size(function gettersize (err, size) {
+  sizeJPEG(function (err) {
     if (err) return finish(err);
 
-    assert.equal(size.width, 460);
-    assert.equal(size.height, 155);
-
-    GM(dir + '/identifyParseErr.jpg').size(function (err) {
+    sizeGIF(function (err) {
       if (err) return finish(err);
-      finish();
+
+      sizePNG(finish);
+    })
+  })
+
+  function sizeJPEG(done) {
+    gm(dir + '/original.jpg')
+    .size(function gettersize (err, size) {
+      if (err) return done(err);
+
+      assert.equal(size.width, 460);
+      assert.equal(size.height, 155);
+
+      gm(dir + '/identifyParseErr.jpg').size(done);
     });
-  });
+  }
+
+  function sizeGIF(done) {
+    gm(dir + '/original.gif')
+    .size(function (err, size) {
+      if (err) return done(err);
+
+      assert.equal(size.width, 192)
+      assert.equal(size.height, 56)
+
+      done()
+    })
+  }
+
+  function sizePNG(done) {
+    gm(dir + '/original.png')
+    .size(function (err, size) {
+      if (err) return done(err);
+
+      assert.equal(size.width, 460)
+      assert.equal(size.height, 155)
+
+      done()
+    })
+  }
 }
