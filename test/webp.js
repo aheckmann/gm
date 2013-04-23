effe7223938c3a1f1a02177e8b635e730edccf63
Jonathan Ong <jonathanrichardong@gmail.com>
2013-04-23 03:20:04 -0700

    tests; add a webp test

---
+++ test/webp.js
@@ -0,0 +1,52 @@
+var assert = require('assert')
+var fs = require('fs')
+
+module.exports = function (_, dir, finish, gm) {
+  if (!gm.integration) return finish()
+
+  if (!_._options.imageMagick) {
+    console.log('GraphicsMagick currently does not support webp :(')
+    return finish()
+  }
+
+  var image = dir + '/original.png'
+
+  write(function (err) {
+    if (err) return finish(err)
+
+    stream(finish)
+  })
+
+  function write(done) {
+    gm(image)
+    .write(dir + '/original.x.webp', function (err) {
+      if (err) return done(err)
+
+      gm(dir + '/original.x.webp').identify(function (err, value) {
+        if (err) return done(err)
+
+        assert.ok(value)
+        console.log(value)
+        assert.equal(value.format = 'webp')
+
+        done()
+      })
+    })
+  }
+
+  function stream(done) {
+    gm(image)
+    .stream('webp', function (err, stdout) {
+      if (err) return done(err)
+
+      gm(stdout).identify(function (err, value) {
+        if (err) return done(err)
+
+        assert.ok(value)
+        assert.equal(value.format = 'webp')
+
+        done()
+      })
+    })
+  }
+}
