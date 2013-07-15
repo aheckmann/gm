c9417cf878fb772c4ca48450e7d71403e4a04fc6
tornillo <vint@tornillo.ru>
2013-07-16 02:07:24 +0400

    allow to pass options as string or object

--- test/getterFormatCustom.js
+++ test/getterFormatCustom.js
@@ -5,15 +5,25 @@ module.exports = function (gm, dir, finish, GM) {
   if (!GM.integration)
     return finish();
 
-  gm
-  .format({ custom: '%f: %m, %wx%h' }, function getterformatcustom (err, format) {
-    if (err) return finish(err);
+  var validResult = 'original.jpg: JPEG, 460x155';
 
-    var validResult = 'original.jpg: JPEG, 460x155';
+  // pass string test
+  gm
+  .format('%f: %m, %wx%h', function getterformatcustom (err, format) {
+    if (err) return finish(err);
 
     assert.equal(format, validResult);
     assert.equal(gm.data.format, validResult);
 
-    finish();
+    // pass object test
+    gm
+    .format({ custom: '%f: %m, %wx%h' }, function getterformatcustom (err, format) {
+      if (err) return finish(err);
+
+      assert.equal(format, validResult);
+      assert.equal(gm.data.format, validResult);
+
+      finish();
+    });
   });
 }
