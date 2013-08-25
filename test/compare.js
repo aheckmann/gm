9fa564ea595e98ef059124c34dc14d9fbef743b5
Cheng Lou <chenglou92@gmail.com>
2013-08-25 02:38:49 -0400

    add diff image output functionality and tests

---
+++ test/compare.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var fs = require('fs');
+
+module.exports = function (gm, dir, finish, GM) {
+  GM.compare(dir + '/original.jpg', dir + '/original.png', function(err, same) {
+    if (err || !same) finish(err);
+    else outputDiff();
+  });
+
+  function outputDiff() {
+    var options = {
+      highlightColor: 'yellow',
+      file: dir + '/diff.png'
+    };
+    GM.compare(dir + '/original.jpg', dir + '/noise1.png', options, function(err) {
+      if (err) return finish(err);
+      fs.exists(options.file, function(exists) {
+        finish(!exists);
+      });
+    });
+  }
+};
