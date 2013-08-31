e0d98ca3bac6f690efa4a80a8100ea5138c8aba8
Cheng Lou <chenglou92@gmail.com>
2013-08-31 15:18:20 -0400

    use a deterministic image to compare with

--- test/compare.js
+++ test/compare.js
@@ -12,7 +12,7 @@ module.exports = function (gm, dir, finish, GM) {
       highlightColor: 'yellow',
       file: dir + '/diff.png'
     };
-    GM.compare(dir + '/original.jpg', dir + '/noise1.png', options, function(err) {
+    GM.compare(dir + '/original.jpg', dir + '/originalCompare.jpg', options, function(err) {
       if (err) return finish(err);
       fs.exists(options.file, function(exists) {
         finish(!exists);
