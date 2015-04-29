4343719948fb15fd59ab2537a0b3f6f4742a2243
encima <encima@gmail.com>
2015-04-29 14:54:13 +0100

    improved checking of whether imagemagick is enabled;

--- test/minifyIM.js
+++ test/minifyIM.js
@@ -2,19 +2,20 @@
 var assert = require('assert')
 
 module.exports = function (gm, dir, finish, GM) {
-  if(!gm._options.imageMagick) return finish();
   var m = gm.minify();
 
   var args = m.args();
   assert.equal('convert', args[0]);
   assert.equal('-minify', args[1]);
 
-  assert.throws(
-    function() {
-      m.write(dir + '/minify.png', function minify (err) { throw err;})
-    },
-    Error
-  );
+  if(gm._options.imageMagick) {
+    assert.throws(
+      function() {
+        m.write(dir + '/minify.png', function minify (err) { throw err;})
+      },
+      Error
+    );
+  }
   return finish();  
 
 }
