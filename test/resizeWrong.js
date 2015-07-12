dd814261e8f78f5a93a064904986603dd2149dee
Dan Rocha <drochag1@gmail.com>
2015-07-12 00:04:11 -0500

    Improved code

--- test/resizeWrong.js
+++ test/resizeWrong.js
@@ -7,36 +7,30 @@ module.exports = function (_, dir, finish, gm) {
 
   var original = dir + '/original.png';
   var resized = dir + '/resize';
-  var widths = [600, 700, 400, 800], i, cb;
+  var widths = [300, 700, 400, 800, 200], i, cb;
+  var resize = function (width, index) {
+    var name = resized + index + '.png';
 
-  var resize = function (width, resized, cb) {
-    console.log('resized', resized);
-    console.log('width', width);
+    if (index == widths.length) {
+      return finish();
+    } else {
+      index++;
+    }
     gm(original)
       .resize(width)
-      .write(resized, function(err){
+      .write(name, function(err){
         if (err) return finish(err);
 
-        gm(resized)
+        gm(name)
           .size(function (err, size) {
             if (err) return finish(err);
             if (size.width !== width) return finish("Wrong resizing on requested:" + width + ", resized:" + size.width);
 
-            if (cb) cb();
-            else finish();
+            if (cb) return cb();
+            resize(widths[index], index);
           });
       });
   };
 
-  for (i = 0; i < widths.length; i++) {
-    if (i < widths.length - 2) {
-      cb = function () {
-        resize(widths[i + 1]);
-      };
-    } else {
-      cb = null;
-    }
-
-    resize(widths[i], resized + i + '.png', cb);
-  };
+  resize(widths[0], 0);
 }
