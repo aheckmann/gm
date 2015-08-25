4fa2418126dc49df162a28fd3c8d5e59bfbc0b91
Dan Rocha <drochag1@gmail.com>
2015-08-25 00:01:55 -0500

    closes #417

--- examples/thumbExact.js
+++ examples/thumbExact.js
@@ -5,7 +5,7 @@ var gm = require('../')
   , dir = __dirname + '/imgs'
 
 gm(dir + '/original.jpg')
-  .thumb(150, 50, dir + '/thumb.jpg', function(err){
+  .thumbExact(150, 50, dir + '/thumb.jpg', function(err){
     if (err) return console.dir(arguments)
     console.log(this.outname + " created  ::  " + arguments[3])
   });
