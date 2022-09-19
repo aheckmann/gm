d0c39abf5d0bdf686373ac824fecdb262abef145
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2022-09-18 21:05:57 -0700

    add test for 714

--- test/autoOrient.js
+++ test/autoOrient.js
@@ -9,11 +9,10 @@ module.exports = function (_, dir, finish, gm) {
 
   var filename = dir + '/autoOrient.jpg';
 
-  gm(dir + '/originalSideways.jpg').orientation(function (err, o) {
+  gm(dir + '/originalSideways.jpg').identify(function (err, o) {
     if (err) return finish(err);
 
-    assert.equal('RightTop', o);
-    assert.ok(!! this.data['Profile-EXIF'], 'No Profile-EXIF data found');
+    assert.equal('155x460', o.Geometry);
 
     // this image is sideways, but may be auto-oriented by modern OS's
     // try opening it in a browser to see its true orientation
@@ -25,10 +24,7 @@ module.exports = function (_, dir, finish, gm) {
       gm(stream).identify(function (err, data) {
         if (err) return finish(err);
 
-        assert.equal('Unknown', data.Orientation);
-        assert.ok(! this.data['Profile-EXIF'], 'Profile-EXIF still exists');
         assert.equal('460x155', data.Geometry);
-
         finish(err);
       })
     })
